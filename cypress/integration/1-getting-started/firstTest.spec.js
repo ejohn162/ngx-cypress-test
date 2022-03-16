a7f56d2b3d94ee6e24853abcb65e96edd2d6ca85
Elizabeth <ecjohnson891@gmail.com>
2022-03-16 15:50:06 -0600

    Second test

--- cypress/integration/1-getting-started/firstTest.spec.js
+++ cypress/integration/1-getting-started/firstTest.spec.js
@@ -43,6 +43,27 @@ describe('Our first suite',() => {
 
     })
      
-       
+    it.only(`second test`,() => {
+
+    
+       cy.visit("/");
+       cy.contains("Forms").click();
+       cy.contains("Form Layouts").click();
+
+       cy.get('[data-cy="signInButton"]')
+
+       cy.contains('Sign in')
+       cy.contains('[status="warning"]', 'Sign in')
+
+       cy.get("#inputEmail3")
+         .parents("form")
+         .find("button")
+         .should("contain", "Sign in")
+         .parents("form")
+         .find('nb-checkbox')
+         .click()
+
+
+    })
 
 })
