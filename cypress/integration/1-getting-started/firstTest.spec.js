c1a96061883fe033d7eef9c50443450c9cd2d486
Elizabeth <ecjohnson891@gmail.com>
2022-03-22 16:18:14 -0600

    Checkbox tests

--- cypress/integration/1-getting-started/firstTest.spec.js
+++ cypress/integration/1-getting-started/firstTest.spec.js
@@ -131,7 +131,7 @@ describe('Our first suite',() => {
 
     })
 
-    it.only('assert property', ()=>{
+    it('assert property', ()=>{
 
     cy.visit('/')
     cy.contains('Forms').click()
@@ -144,6 +144,49 @@ describe('Our first suite',() => {
     })
 
     
+    })
+
+    it('radio button', ()=>{
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layout').click()
+
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(radioButtons=>{
+            cy.wrap(radioButtons)
+            .first()
+            .check({force:true})
+            .should('be.checked')
+
+            cy.wrap(radioButtons)
+            .eq(1)
+            .check({force:true})
+
+            cy.wrap(radioButtons)
+            .first()
+            .should('not.be.checked')
+
+            cy.wrap(radioButtons)
+            .eq(2)
+            .should('be.disabled')
+
+        })
+
+
+    })
+
+    it.only('check boxes',()=>{
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+
+        cy.get('[type="checkbox"]').check({force:true})
+        cy.get('[type="checkbox"]').eq(0).click({force:true})
+        cy.get('[type="checkbox"]').eq(2).click({force:true})
+
+
+
+
+
     })
 
 })
